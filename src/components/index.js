ed91134573d00723e4c51f7d3db9a79ee3163510
jaygraygray <jeremygray001@gmail.com>
2021-11-23 14:11:43 -0500

    move layout components to layout directory

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,1 @@
-export { Header } from "./Header";
-export { Footer } from "./Footer";
-export { LeftSideBar } from "./LeftSideBar";
-export { RightSideBar } from "./RightSideBar";
-export { Main } from "./Main";
+export * from "./layout";
---
+++ src/components/layout/index.js
@@ -0,0 +1,5 @@
+export { Header } from "./Header";
+export { Footer } from "./Footer";
+export { LeftSideBar } from "./LeftSideBar";
+export { RightSideBar } from "./RightSideBar";
+export { Main } from "./Main";
